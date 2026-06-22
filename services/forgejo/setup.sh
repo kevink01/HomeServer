1645717c4a8f9c5fe2a40ece60e3c6c230797e10
Kevin Kulich <klpv733@gmail.com>
2026-06-22 18:51:02 -0400

    Update forgejo docker contianer & environment creation

---
+++ services/forgejo/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ENV_FILE=".env"
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo ".env file not found. Creating a new one..."
+    RANDOM_PASS=$(LC_ALL=C tr -dc 'A-Za-z0-9!@#%^&*()-_=+' < /dev/urandom | head -c 32)
+
+    {
+        echo "POSTGRES_DB=forgejo"
+        echo "POSTGRES_USER=forgejo"
+        echo "POSTGRES_PASSWORD=$RANDOM_PASS"
+    } > "$ENV_FILE"
+
+    echo ".env file created successfully."
+else
+    echo ".env file already exists."
+fi
