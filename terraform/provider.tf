a90cc4948101453448a587e52c85f4295403fdae
Kevin Kulich <klpv733@gmail.com>
2026-06-27 13:27:24 -0400

    chore: Update uptime kuma timeout config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,4 +2,7 @@ provider "uptimekuma" {
   endpoint = var.uptime_kuma_endpoint
   username = var.uptime_kuma_username
   password = var.uptime_kuma_password
+  timeout = "2m"
+  max_retries = 20
+  per_attempt_timeout = "5s"
 }
